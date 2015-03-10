a08e0d3f9d09469c7a0e383b4298d55c424a7847
mariliaferreira <mariliaferreiraa@gmail.com>
2015-03-10 08:58:14 -0300

    CSS HTML Formas de Pagamento

---
+++ formas-pagamento.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Grupo Editorial PUCPR - Editora Champagnat, PUCPress e PUCSul</title>
+
+	  <!-- Meta Tags - Informações sobre o Website -->
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <meta name="keywords" content="grupo editorial pucpr, champagnat, pucpress, pucsul, livro, livros, revistas, acadêmico, pucpr">
+	  <meta name="description" content="Grupo Editorial PUCPR - Editora Champagnat, PUCPress e PUCSul">
+	  <meta http-equiv="content-language" content="pt-br">
+	  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	  <meta name="author" content="Grupo Editorial PUCPR">
+	  <meta name="reply-to" content="contato@editorachampagnat.com.br">
+
+
+	  <!-- Arquivos de CSS -->
+	  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+	  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+	  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+	  
+
+	  <!-- Arquivos de Javascript -->
+	  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  	  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+  	  <script src="js/responsive-menu.js"></script>
+  	  <script src="js/tabs.js"></script>
+  	  <script src="js/classie.js"></script>
+
+
+
+  	<!--  <script type="text/javascript">
+	  //   window.onload = function (){
+	  //     var myPDF = new PDFObject({ url: "objetos/examplePDF.pdf" }).embed();
+	  //   };
+	  // </script>-->
+
+	</head>
+
+
+	<body>
+
+		<?php
+		require('componentes/cabecalho.php');
+		?>
+
+		<div class="header-livros">
+			<div class="breadcrumb">
+				<ul>
+					<li><a href=""><i class="fa fa-home"></i></a></li> <span>></span>
+					<li><a href="">Formas de Pagamento</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<h1>formas de pagamento</h1>
+
+		<div class="default-box">
+
+		
+			
+			<ul>
+				<li>
+					<i class="fa fa-barcode"></i> 
+					Boleto Bancário
+				</li>
+				<li>
+					<i class="fa fa-cc-visa"></i> 
+					Cartão de <strong>Crédito VISA</strong>
+				</li>
+				<li>
+					<i class="fa fa-cc-mastercard"></i> 
+					Cartão de <strong>Crédito MasterCard</strong>
+				</li>
+				<li>
+					<i class="fa fa-cc-visa"></i> 
+					Cartão de <strong>Débito VISA</strong>
+				</li>
+				<li>
+					<i class="fa fa-cc-mastercard"></i> 
+					Cartão de <strong>Débito MasterCard</strong>
+				</li>
+				<li>
+					<i class="fa fa-cc-paypal"></i>
+					Paypal<br />
+				</li>
+
+				
+			</ul>
+
+
+		</div>
+
+
+		<?php
+		require('componentes/footer.php');
+		?>
+
+		<script src="js/classie.js"></script>
+		<script src="js/search-checkout.js"></script>
+		
+	</body>
+</html>
