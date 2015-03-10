013d86e4df80236e99f464a1cf3ee38d13ca66d2
mariliaferreira <mariliaferreiraa@gmail.com>
2015-03-10 09:06:43 -0300

    CSS HTML Opções de Entrega

---
+++ opcoes-entrega.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  <title>Grupo Editorial PUCPR - Editora Champagnat, PUCPress e PUCSul</title>
+
+	  <!-- Meta Tags - Informações sobre o Website -->
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+	  <meta name="keywords" content="grupo editorial pucpr, champagnat, pucpress, pucsul, livro, livros, revistas, acadêmico, pucpr">
+	  <meta name="description" content="Grupo Editorial PUCPR - Editora Champagnat, PUCPress e PUCSul">
+	  <meta http-equiv="content-language" content="pt-br">
+	  <meta http-equiv="content-type" content="text/html; charset=iso-8859-1">
+	  <meta name="author" content="Grupo Editorial PUCPR">
+	  <meta name="reply-to" content="contato@editorachampagnat.com.br">
+
+
+	  <!-- Arquivos de CSS -->
+	  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+	  <link rel="stylesheet" type="text/css" href="css/normalize.css">
+	  <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.2.0/css/font-awesome.min.css" />
+	  
+
+	  <!-- Arquivos de Javascript -->
+	  <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+  	  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+  	  <script src="js/responsive-menu.js"></script>
+  	  <script src="js/tabs.js"></script>
+  	  <script src="js/classie.js"></script>
+
+
+
+  	<!--  <script type="text/javascript">
+	  //   window.onload = function (){
+	  //     var myPDF = new PDFObject({ url: "objetos/examplePDF.pdf" }).embed();
+	  //   };
+	  // </script>-->
+
+	</head>
+
+
+	<body>
+
+		<?php
+		require('componentes/cabecalho.php');
+		?>
+
+		<div class="header-livros">
+			<div class="breadcrumb">
+				<ul>
+					<li><a href=""><i class="fa fa-home"></i></a></li> <span>></span>
+					<li><a href="">Opções de Entrega</a></li>
+				</ul>
+			</div>
+		</div>
+
+		<h1>opções de entrega</h1>
+
+		<div class="default-box">
+			
+			<ul>
+				<li>
+					<i class="fa fa-truck"></i> 
+					Entrega <strong>PAC Correios</strong>
+				</li>
+				<li>
+					<i class="fa fa-plane"></i>
+					Entrega <strong>SEDEX</strong>
+				</li>
+								
+			</ul>
+
+
+		</div>
+
+
+		<?php
+		require('componentes/footer.php');
+		?>
+
+		<script src="js/classie.js"></script>
+		<script src="js/search-checkout.js"></script>
+		
+	</body>
+</html>
